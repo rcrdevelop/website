2efd82d98b3c73c9cbaf941315260579b05568aa
rcrdevelop <contreras06rj@gmail.com>
2025-01-27 17:22:48 -0400

    update experience

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -8,25 +8,8 @@ export const EXPERIENCE: ExperienceProp[] = [
     companyUrl: "https://github.com/rcrdevelop/index-context",
     description:
       "Diseño y desarrollo de una herramienta SaaS para gestionar informacion compleja de múltiples tiendas en tiempo real. Utilicé Next y Java para la sincronización y Tailwind CSS para una interfaz atractiva y funcional.",
-    technologies: ["Next", "Java", "Tailwind CSS", "Openai"],
+    technologies: ["Next", "Tailwind CSS", "Openai"],
     logo: "/logos/nextjs-icon-dark-background.svg",
-  },
-  {
-    dates: "Ecommerce",
-    title: "Shopify",
-    company: "FullStack",
-    companyUrl: "https://headless.rcrdevelop.com",
-    description:
-      "Diseño y desarrollo de una solución completa de eCommerce para Dutch Pet utilizando Shopify Storefront API y Admin API. Implementé integraciones avanzadas como ReCharge para suscripciones, y optimicé la experiencia de usuario a través de layouts y flujos pixel-perfect creados en colaboración con el equipo de diseño.",
-    technologies: [
-      "Shopify Storefront API",
-      "Shopify Admin API",
-      "Nuxt",
-      "Node.JS",
-      "Liquid",
-      "Tailwind",
-    ],
-    logo: "/logos/shopify_glyph_white.svg",
   },
   {
     dates: "Saas",
@@ -40,15 +23,5 @@ export const EXPERIENCE: ExperienceProp[] = [
       "Firebase"
     ],
     logo: "/logos/Firebase Realtime Database_Standalone logomark.svg",
-  },
-  {
-    dates: "Template",
-    title: "Custom Shopify Theme",
-    company: "Frontend",
-    companyUrl: "https://store.rcrdevelop.com",
-    description:
-      "Creación de un tema personalizado para Shopify, optimizado para SEO y alto rendimiento. El proyecto incluyó diseño desde cero con Liquid y Tailwind CSS, priorizando la velocidad de carga y la usabilidad.",
-    technologies: ["Shopify", "Liquid", "Tailwind CSS"],
-    logo: "/logos/shopify_glyph_white.svg",
   }
 ];
